5fef5f9f1e5b074df42e58b676865c55b030b4e9
Juraj Volsini <volesinijurajml@gmail.com>
2025-03-23 15:54:46 +0100

    git push origin main

--- Kontinuace/gauss.f90
+++ Kontinuace/gauss.f90
@@ -1,38 +1,108 @@
-subroutine gause(n, n1, nmax, a, b, delta, tanv, kfix, ierr)
-    ! Modernized Fortran code
-    use, intrinsic :: iso_fortran_env, only: dp => real64
-    implicit none
-    
-    ! Arguments
-    integer, intent(in) :: n, n1, nmax
-    real(dp), intent(inout) :: a(nmax, nmax+1), b(nmax+1)
-    real(dp), intent(out) :: delta(nmax+1), tanv(nmax+1)
-    integer, intent(out) :: kfix, ierr
-  
-    ! Local variables
-    real(dp) :: bold(nmax+1), y(nmax+1), x(nmax+1)
-    integer :: irr(nmax+1), irk(nmax+1), i, j, k
-  
-    ! Ensure numerical safety
-    ierr = 0
-    
-    ! Gaussian elimination with pivoting
-    do i = 1, n
-      ! Pivot selection (basic implementation, might need improvement)
-      if (abs(a(i,i)) < 1.0e-12_dp) then
+subroutine gauss(n, n1, nmax, a, b, delta, tanv, kfix, ierr)
+  implicit none
+  ! Input parameters
+  integer, intent(in) :: n, n1, nmax
+  ! Inout/output parameters
+  integer, intent(out) :: kfix, ierr
+  double precision, intent(inout) :: a(nmax, nmax+1), b(nmax+1)
+  double precision, intent(out) :: delta(nmax+1), tanv(nmax+1)
+
+  ! Local variables and arrays
+  double precision :: pref(nmax+1), bold(nmax+1)
+  double precision :: y(nmax+1), x(nmax+1)
+  integer :: irr(nmax+1), irk(nmax+1)
+  integer :: i, j, id, ir, is
+  double precision :: amax, p, s
+
+  ! Save a copy of the right-hand side vector
+  do i = 1, n1
+     bold(i) = b(i)
+  end do
+
+  ierr = 0
+  do i = 1, n1
+     irk(i) = 0
+     irr(i) = 0
+     pref(i) = 1.0d0
+  end do
+
+  ! Gaussian elimination with pivoting
+  do id = 1, n
+     ir = 1
+     is = 1
+     amax = 0.0d0
+     do i = 1, n
+        if (irr(i) == 0) then
+           do j = 1, n1
+              p = pref(j) * abs(a(i, j))
+              if (p > amax) then
+                 ir = i
+                 is = j
+                 amax = p
+              end if
+           end do
+        end if
+     end do
+
+     if (amax == 0.0d0) then
         ierr = 1
         return
-      end if
-      
-      do j = i+1, n
-        a(j, :) = a(j, :) - (a(j, i) / a(i, i)) * a(i, :)
-      end do
-    end do
-    
-    ! Back-substitution
-    do i = n, 1, -1
-      delta(i) = (b(i) - sum(a(i, i+1:n) * delta(i+1:n))) / a(i, i)
-    end do
-  
-  end subroutine gause
-  
+     end if
+
+     irr(ir) = is
+     do i = 1, n
+        if (i /= ir .and. a(i, is) /= 0.0d0) then
+           p = a(i, is) / a(ir, is)
+           do j = 1, n1
+              a(i, j) = a(i, j) - p * a(ir, j)
+           end do
+           a(i, is) = 0.0d0
+           b(i) = b(i) - p * b(ir)
+        end if
+     end do
+  end do
+
+  ! Back substitution and computing Newton corrections
+  do i = 1, n
+     ir = irr(i)
+     x(ir) = b(i) / a(i, ir)
+     irk(ir) = 1
+  end do
+
+  ! Find the index of the independent variable (one not used as a pivot)
+  do i = 1, n1
+     if (irk(i) == 0) then
+        kfix = i
+     end if
+  end do
+
+  do i = 1, n
+     ir = irr(i)
+     y(ir) = -a(i, kfix) / a(i, ir)
+  end do
+
+  do i = 1, n1
+     b(i) = x(i)
+     delta(i) = y(i)
+  end do
+
+  b(kfix) = 0.0d0
+  delta(kfix) = 1.0d0
+
+  s = 0.0d0
+  do i = 1, n1
+     s = s + delta(i)**2
+  end do
+  do i = 1, n1
+     delta(i) = delta(i) / sqrt(s)
+  end do
+
+  ! Set the tangent vector and restore b
+  do i = 1, n1
+     tanv(i) = delta(i)
+     delta(i) = b(i)
+     b(i) = bold(i)
+  end do
+
+  return
+end subroutine gauss
--- Kontinuace/kontinuace.f90
+++ Kontinuace/kontinuace.f90
@@ -9,25 +9,96 @@ program kontinuace
     dimension f(1:nmax), J(1:nmax, 1:nmax+1)
     real(8) :: y
     dimension y(1:nmax+1) ! pre-made array of size 1:nmax+1
+    real(8) :: delta, tanv
+    dimension delta(1:nmax+1), tanv(1:nmax+1)
+    integer :: kfix, ierr
+    integer :: inewt, maxnewt, ieul, maxeul
+    real(8) :: norm, eps, step, minstep, maxstep
 
     real(8) :: gama, B, lambda, thetaC, beta
     COMMON /pars/ gama, B, lambda, thetaC, beta 
 
     ! Load data & assign data
-    OPEN (10, file = 'input.txt')
+    OPEN(10, file ='input.txt')
+    OPEN(11, file='output.txt')
 
-    READ (10,*) ndim, (y(i), i=1,ndim+1)
+    READ(10,*) ndim, (y(i), i=1,ndim+1)
     READ(10,*) gama, B, lambda, thetaC, beta
+    READ(10,*) maxnewt, eps
+    READ(10,*) maxeul
+    READ(10,*) step, minstep, maxstep
 
+    ! Euler cycle
+    do ieul = 1, maxeul
+        ! Newton cycle
+        do inewt = 1,maxnewt
+            WRITE(*,*) 'Newton iteration no.', inewt
 
+            ! Create right hand sides
+            call RHS(ndim, nmax, y, f, J)
+            WRITE(*,*) 'y= ', (y(i), i=1,ndim+1) 
+            WRITE(*,*) 'f= ', (f(i), i=1,ndim)
+            WRITE(*,*) 'J= ', (j(1,i), i=1,ndim+1)
+            WRITE(*,*) '   ', (j(2,i), i=1,ndim+1)
 
-    call RHS(ndim, nmax, y, f, J)
-    WRITE(*,*) 'y=', (y(i), i=1,ndim+1) 
-    WRITE(*,*) 'f=', (f(i), i=1,ndim)
-    WRITE(*,*) 'j=', (j(1,i), i=1,ndim+1)
-    WRITE(*,*) '  ', (j(2,i), i=1,ndim+1)
+            ! Call gauss elimination to solve the eq.
+            call gauss(ndim, ndim+1, nmax, J, f, delta, tanv, kfix, ierr)
+            WRITE(*,*) 'delta= ', (delta(i), i=1,ndim+1)
+            
+            ! Corections
+            do i=1,ndim+1
+                y(i) = y(i) - delta(i)
+            end do
+            WRITE(*,*) 'y= ', (y(i), i=1,ndim+1)
+            
+            norm = 0.0
+            do i=1,ndim+1
+                norm = norm + delta(i)**2
+            end do
+            norm = norm**0.5
+            WRITE(*,*) 'norm =', norm
 
-    CLOSE (10)
+            if (norm .le. eps) then
+                WRITE(*,*) 'Newton OK'
+                exit
+            else
+                WRITE (*,*) 'Newton did not converge!'
+            end if
+        end do
+
+        write(11,*) (y(i),i=1,ndim+1)
+
+        ! Adaptive step
+        if (inewt .eq. 1) step = 2.0*step
+        if (inewt .eq. 2) step = 1.5*step
+        if (inewt .eq. 4) step = 0.8*step
+        if (inewt .ge. 5) step = 0.5*step
+        if (step .le. minstep) step = minstep
+        if (step .ge. maxstep) step = maxstep
+        write(*,*)'Newton step =', step
+
+        ! Create right hand sides
+        call RHS(ndim, nmax, y, f, J)
+        WRITE(*,*) 'y= ', (y(i), i=1,ndim+1) 
+        WRITE(*,*) 'f= ', (f(i), i=1,ndim)
+        WRITE(*,*) 'J= ', (j(1,i), i=1,ndim+1)
+        WRITE(*,*) '   ', (j(2,i), i=1,ndim+1)
+
+        WRITE(*,*) 'Euler step no.', ieul
+        
+        ! Call gauss elimination to solve the eq.
+        call gauss(ndim, ndim+1, nmax, J, f, delta, tanv, kfix, ierr)
+        WRITE(*,*) 'tanv= ', (tanv(i),i=1,ndim+1)
+
+        ! Step correction
+        do i = 1, ndim+1
+            y(i) = y(i) + tanv(i)*step
+        end do
+        WRITE(*,*) 'y= ', (y(i),i=1,ndim+1)
+    end do
+
+    CLOSE(10)
+    CLOSE(11)
 
 end program kontinuace
 
